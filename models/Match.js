d847c38c7914748cdb5a78932351e8d8bbdae098
heer <heerpatel291@gmail.com>
2024-10-19 22:25:56 -0400

    changes-01

---
+++ models/Match.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const matchSchema = new Schema({
+  sessionId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Session' 
+  },
+  matches: {
+    type: Map,
+    of: [{
+      worker: { type: Number},
+      customer: { type: Number },
+      pretip:{type:Number},
+      // posttip:{type:Number},
+      effort:{type:Number},
+      customerSet:{type:Number},
+      totalCompWorker:{type:Number},
+      totalCompCustomer:{type:Number}
+    }]
+  }
+});
+
+module.exports = mongoose.model('Match', matchSchema);
