a5e8930dc191d79ac52ed1aec6dfa511c169f5b4
heer <heerpatel291@gmail.com>
2024-10-20 23:12:29 -0400

    changes

--- routes/GenerateLink.js
+++ routes/GenerateLink.js
@@ -21,7 +21,7 @@ router.route("/link").post(async (req, res) => {
     const { participants, condition } = req.body;
 
     const uniqueLinkID = Math.random().toString(36).substring(7);
-    const link = `https://project-sfa-frontend.vercel.app/link/${uniqueLinkID}`;
+    const link = `https://project-sfa-frontend-1.onrender.com/link/${uniqueLinkID}`;
 
     const round = 10;
     const savedSession = new Sessions({
