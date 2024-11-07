0c37e1fe3c8f17c8c4d7a4bcad9f70c7476bb9de
heer <heerpatel291@gmail.com>
2024-11-06 23:53:58 -0500

    changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 // Configure CORS to allow requests from your frontend
 app.use(cors({
-    origin: 'http://18.118.112.50',
+    origin: 'http://3.22.116.167',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true
 }));
@@ -41,7 +41,7 @@ const server = http.createServer(app);
 // Configure Socket.io server with proper CORS settings
 const io = new Server(server, {
     cors: {
-        origin: 'http://18.118.112.50',
+        origin: 'http://3.22.116.167',
         methods: ["GET", "POST"],
         credentials: true
     }
--- routes/GenerateLink.js
+++ routes/GenerateLink.js
@@ -21,7 +21,7 @@ router.route("/link").post(async (req, res) => {
     const { participants, condition } = req.body;
 
     const uniqueLinkID = Math.random().toString(36).substring(7);
-    const link = `http://18.118.112.50/link/${uniqueLinkID}`;
+    const link = `http://3.22.116.167/link/${uniqueLinkID}`;
 
     const round = 10;
     const savedSession = new Sessions({
