f3cd707e60ddbec75872ff2a110a72b63aec0dce
heer <heerpatel291@gmail.com>
2024-11-14 12:07:30 -0500

    changes

--- routes/GenerateLink.js
+++ routes/GenerateLink.js
@@ -1343,26 +1343,20 @@ router.post('/saveresponsesforscreen23', async (req, res) => {
       const sessionObj = await Sessions.findOne({ link });
       const sessionId = sessionObj._id.toHexString();
 
-      let response = await Response.findOne({ pnumber, sessionId });
+      const existingResponse = await Response.findOne({ pnumber, sessionId });
 
-      if (!response) {
-        response = new Response({
-          pnumber,
-          sessionId,
-          condition,
-          TipReason_Effort,
-          TipReason_SocialImage,
-          TipReason_SocialNorm
-        });
-      } else {
-        response.TipReason_Effort = TipReason_Effort;
-        response.TipReason_SocialImage = TipReason_SocialImage;
-        response.TipReason_SocialNorm = TipReason_SocialNorm;
+      if (!existingResponse) {
+        return res.status(404).send({ msg: "Response not found" });
       }
+      existingResponse.controllability1 = Controllability1;
+      existingResponse.controllability2 = Controllability2;
+      existingResponse.TipReason_Effort = TipReason_Effort;
+      existingResponse.TipReason_SocialImage = TipReason_SocialImage;
+      existingResponse.TipReason_SocialNorm = TipReason_SocialNorm;
 
-      await response.save();
+      await existingResponse.save();
 
-      res.status(200).send({ msg: 'Response saved successfully' });
+      res.status(200).send({ msg: 'Response updated successfully' });
     } catch (error) {
       console.error("Error saving response:", error);
       res.status(500).send({ msg: "Internal server error" });
@@ -1388,26 +1382,33 @@ router.post('/saveresponsesforscreen24', async (req, res) => {
       const sessionObj = await Sessions.findOne({ link });
       const sessionId = sessionObj._id.toHexString();
 
-      const existingResponse = await Response.findOne({ pnumber, sessionId });
+      let response = await Response.findOne({ pnumber, sessionId });
 
-      if (!existingResponse) {
-        return res.status(404).send({ msg: "Response not found" });
+      if (!response) {
+        response = new Response({
+          pnumber,
+          sessionId,
+          condition,
+          TipReason_Effort,
+          TipReason_SocialImage,
+          TipReason_SocialNorm
+        });
+      } else {
+        response.TipReason_Effort = TipReason_Effort;
+        response.TipReason_SocialImage = TipReason_SocialImage;
+        response.TipReason_SocialNorm = TipReason_SocialNorm;
       }
-      existingResponse.controllability1 = Controllability1;
-      existingResponse.controllability2 = Controllability2;
-      existingResponse.TipReason_Effort = TipReason_Effort;
-      existingResponse.TipReason_SocialImage = TipReason_SocialImage;
-      existingResponse.TipReason_SocialNorm = TipReason_SocialNorm;
 
-      await existingResponse.save();
+      await response.save();
 
-      res.status(200).send({ msg: 'Response updated successfully' });
+      res.status(200).send({ msg: 'Response saved successfully' });
     } catch (error) {
       console.error("Error saving response:", error);
-      res.status(500).send({ msg: "Internal server error" });
+      res.status(200).send({ msg: "Internal server error" });
     }
   });
 });
+
 
 router.post('/postresponse', async (req, res) => {
   const token = req.body.token;
