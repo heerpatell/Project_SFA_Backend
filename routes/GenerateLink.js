bd57e14d2bf137cde21ac2ef8400dee490988c43
heer <heerpatel291@gmail.com>
2024-11-18 14:07:45 -0500

    changes

--- routes/GenerateLink.js
+++ routes/GenerateLink.js
@@ -620,7 +620,7 @@ router.post('/addeffortlevel', async (req, res) => {
     } 
     else if(condition == 'Pre-Tip'){
       const workerTip = entry.pretip || 0; // Ensure tip is a number
-      entry.totalCompWorker = 160 + Number(workerTip) - Number(effortToTokens); // Calculate total compensation for worker
+      entry.totalCompWorker = 160 + Number(workerTip); // Calculate total compensation for worker
       entry.totalCompCustomer = 60 + Number(effortlevel * 200) - Number(workerTip); // Calculate total compensation for customer
       console.log(625,entry.totalCompCustomer)
       console.log(626,entry.totalCompWorker)
